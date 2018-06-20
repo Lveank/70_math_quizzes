434f02096d1ca6818f8e5ff43a31074dd0014567
Lveank <zhaoyichen71@gmail.com>
2018-06-20 23:32:12 +0800

    修正： global count

--- 05_还在用现金支付吗（递归版）.py
+++ 05_还在用现金支付吗（递归版）.py
@@ -5,11 +5,8 @@
 注：不计硬币兑出的先后顺序；只考虑10、50、100、500日元硬币的情况；最多只能兑出15枚硬币。
 """
 
-count = []
+count = 0
 
-
-# a*10 + b*50 + c*100 + d*500 = 1000
-# a+b+c+d <= 15
 
 def change(target, coins, usable):
     """
@@ -17,17 +14,15 @@ def change(target, coins, usable):
     coins：可找面值，列表
     usable：总硬币数
     """
-    coin = coins.pop()  # list.pop([index = -1]) 移除列表中的一个元素（默认最后一个元素），并且返回该元素的值
-    if len(coins) == 0:
+    global count
+    if len(coins) != 0:
+        coin = coins.pop(0)  # list.pop([index = -1]) 移除列表中的一个元素（默认最后一个元素），并且返回该元素的值
         if int(target / coin) <= usable:
-            # count += 1
-            return 1
+            count += 1
         else:
             for i in range(0, int(target / coin)):
                 change(target - coin * i, coins, usable - 1)
 
 
-coins = [10, 50, 100, 500]
-a = change(1000, coins, 15)
-count.append(a)
+change(1000, [10, 50, 100, 500], 15)
 print(count)
